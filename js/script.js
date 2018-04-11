2e045ac4b07091b7bf00c62e5bf217e52ba52677
hephest <hephaestum@hotmail.com>
2018-04-11 20:04:42 +0300

    Added logo and color theme, fixed both nav versions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+    //Sidenav initialisation
     $('.sidenav').sidenav();
+    //Dropdown initialisation
     $(".dropdown-trigger").dropdown();
+    //Collapsible initialization
+    $('.collapsible').collapsible();
 })
+
+//Change icon for collapsible header 'Information'
+/*function changeIcon() {
+    el = document.getElementById("info-collapsible-list");
+    if(el.textContent === "arrow_drop_down") {
+        el.innerHTML = "arrow_drop_up";
+    } else {
+        el.innerHTML = "arrow_drop_down";
+    }
+}
+*/
